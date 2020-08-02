7652ff58f0ccd2d80a358c7c451322f2279a66aa
Rajat-Chowdhury <crajat2000@gmail.com>
2020-08-02 13:01:32 +0530

    performance of app improved. module 19 completed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import {connect} from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+
+const asyncCheckout = asyncComponent(() => {
+  return import ('./containers/Checkout/Checkout');
+});
+const asyncOrders = asyncComponent(() => {
+  return import ('./containers/Orders/Orders');
+});
+const asyncAuth = asyncComponent(() => {
+  return import ('./containers/Auth/Auth');
+});
 
 class App extends Component {
 
@@ -20,7 +29,7 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/auth" exact component={Auth} />
+        <Route path="/auth" exact component={asyncAuth} />
         <Redirect to = "/" />
       </Switch>
       
@@ -28,9 +37,10 @@ class App extends Component {
 
     if ( this.props.isAuthenticated){
       routes = (
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
+        <Switch>
+            <Route path="/checkout" component={asyncCheckout} />
+            <Route path="/orders" component={asyncOrders} />
+            <Route path="/auth" exact component={asyncAuth} />
             <Route path="/logout" exact component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
